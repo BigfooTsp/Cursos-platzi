b9cdc88f8d218957b7ca338c81fdfadb58a7eea8
Pedro Pina <pedrojosepinacuenca@gmail.com>
2019-03-18 07:27:13 +0100

    Activando buscador
    Se activa el buscador tras cargar las listas y gestiona la búsqueda. Se incorpora el uso de extensión 'Coment anchors' para comentarios con VSC con etiquetas

--- De-JQuery-a-JS/Prácticas/Proyecto-1/script.js
+++ De-JQuery-a-JS/Prácticas/Proyecto-1/script.js
@@ -1,56 +1,47 @@
 
-const VIDEO_API = 'https://yts.am/api/v2/list_movies.json?' // https://yts.am/api/v2/list_movies.json?sort=seeds&limit=15
-const genres = ['comedy', 'sci-fy', 'horror', 'action', 'drama']
 const USER_API = 'https://randomuser.me/api/'
-const $moviesSection = document.getElementsByClassName('movies-section')
-const $movieList = document.getElementsByClassName('movies-list')
-const $movieBox = document.getElementsByClassName('movie-box')
-const $searchBox = document.getElementsByClassName('buscador')
-const $modal = document.getElementsByClassName('modal')
 const $miPlaylist = document.getElementsByClassName('miPlaylist-list')
-const $friend = document.getElementsByClassName('friend-container')
+const $friendsList= document.getElementsByClassName('friend-container')
 
-async function loadLists() {
-    // Muestra las listas de peliculas
-    async function createCache(genre) {
-        // Descarga las listas de la API y las guarda en localStorage
-        const response = await fetch(`${VIDEO_API}genre=${genre}`)
-        const {data: {movies: data}} = await response.json()
-        window.localStorage.setItem(genre, JSON.stringify(data)) // solo guarda texto
-    }
-    async function createList(genre) {
-        // Crea las listas en localStorage si no lo están ya y devuelve su valor en JSON
-        let cacheList = window.localStorage.getItem(genre)
-        if (!cacheList) { 
-            await createCache(genre)
-        }
-        cacheList = window.localStorage.getItem(genre)
-        return JSON.parse(cacheList)
-    }
+async function callAPI(params) {                        // STUB Gestiona llamada a API externa y guarda en localStorage
+    try {
+        const VIDEO_API = 'https://yts.am/api/v2/list_movies.json?'
+        const resp= await fetch(`${VIDEO_API}${params}`)
+        const miJson = await resp.json()
 
-    function ListTemplate(cacheList, genre) {
-        // Devuelve el template HTML del contenedor de la lista.
-        return `<div class="movies-list" id='movie-list-${genre}'>
+        if (miJson.data.movie_count) {
+            const {data: {movies: data}} = miJson
+            data.forEach(movie => {
+                const title = movie.title
+                window.localStorage.setItem(title, JSON.stringify(movie)) // NOTE solo guarda texto   
+            }) 
+            return data
+        } 
+    } catch (error) {
+        console.log('callAPI function error', error)
+    }
+}   
+
+async function loadLists() {                            // SECTION Carga las listas
+    function ListTemplate(cacheList, genre) {           // NOTE Devuelve el template HTML del contenedor de la lista.
+        return `<div class="movies-list"'>
                     <h1>${genre}</h1>
                     <div class="movie-box-container">
                         ${cacheList}
                     </div>
                 </div>`
     }
-    function MovieTemplate(movie, genre) {
-        // Devuelve el template HTML con el contenedor de la película.
-        return `<div class="movie-box" data-id=${movie.id} data-genre=${genre}>
+    function MovieTemplate(movie, genre) {              // NOTE Devuelve el template HTML con el contenedor de la película.
+        return `<div class="movie-box" data-id=${movie.id} data-genre="${genre}" data-title="${movie.title}">
                     <figure class="movie-image-container">
                         <img src="${movie.medium_cover_image}" alt="">
                     </figure>
                     <p class="movie-title">${movie.title}</p>
                 </div>`
     }
-    async function getTemplate(genre) {
-        // Devuelve el template del género completo en texto
-        let cacheList = await createList(genre) // Obtiene el JSON con los datos del género.
+    async function getTemplate(genre) {                 // NOTE Realiza consulta y evuelve el template del género completo en texto
+        let cacheList = await callAPI(`genre=${genre}`) // NOTE Obtiene el JSON con los datos del género.
         let HTMLString = ''
-        
         cacheList.forEach(movie => {
             let HTMLMovieItem = MovieTemplate(movie, genre)
             HTMLString += HTMLMovieItem
@@ -58,24 +49,103 @@ async function loadLists() {
         return ListTemplate(HTMLString, genre)
     }
 
-    // Procesando las listas ... No puede ser en un bucle :(
-    let HTMLTemplate = ``
-    HTMLTemplate += await getTemplate('comedy')
-    HTMLTemplate += await getTemplate('sci-fy')
-    HTMLTemplate += await getTemplate('horror')
-    HTMLTemplate += await getTemplate('action')
-    HTMLTemplate += await getTemplate('drama')
-    // Pegando html
-    $moviesSection[0].innerHTML = HTMLTemplate
-    // Mostrando con animación fade-in
-    const imageList = Array.from($moviesSection[0].querySelectorAll('img'))
-    imageList.forEach(image => {
-        image.addEventListener('load', event => {
-            event.srcElement.classList.add('fadeIn')
+    Procesando_Listas: {                                // NOTE Procesando las listas ... No puede ser en un bucle del array genres:(
+        const $moviesSection = document.getElementsByClassName('movies-section')
+        let HTMLTemplate = ``                               
+        HTMLTemplate += await getTemplate('comedy')
+        HTMLTemplate += await getTemplate('sci-fy')
+        HTMLTemplate += await getTemplate('horror')
+        HTMLTemplate += await getTemplate('action')
+        HTMLTemplate += await getTemplate('drama')
+        
+        $moviesSection[0].innerHTML = HTMLTemplate          // STUB Pegando html
+        const imageList = Array.from($moviesSection[0].querySelectorAll('img'))    // STUB Mostrando con animación fade-in
+        imageList.forEach(image => {
+            image.addEventListener('load', event => {       // STUB Listener
+                event.srcElement.classList.add('fadeIn')
+            })
+            let container = image.parentElement.parentElement
+            container.addEventListener('click', manageSearch)
         })
-    })
+    }
+}                                                       //!SECTION 
+
+async function manageSearch(event) {                    // SECTION Activa y gestiona búsqueda
+    function modalTemplate(movie) {                     // NOTE Crea el template con la información de la película
+        return `<h2 class="title">${movie.title}</h2>
+                <figure class = 'image'>
+                    <img src="${movie.medium_cover_image}" alt="">
+                </figure>
+                <p class="description">${movie.description_full}</p>
+                <button id='modal-close-Button' type="">Cerrar</button>`
+    }
+    function modalNotFound() {                          // NOTE Crea el template con el mensaje de no encontrado.
+        return `<h2 class="title">Película no encontrada</h2>
+                <figure class = 'image'>
+                    <img src="images/notFound.png" alt="">
+                </figure>
+                <button id='modal-close-Button' type="">Cerrar</button>`
+    }
+    
+    async function searchMovie(searchString) {          // NOTE Devuelve el template tras la búsqueda externa    
+        let template = window.localStorage.getItem(searchString)    // STUB Comprueba en cache
+        let data
+
+        if (template) {
+            template = JSON.parse(template)
+            return modalTemplate(template)
+        } else {
+            data = await callAPI(`limit=1&query_term='${searchString}'`)
+            if(data) {
+                return modalTemplate(data[0])
+            } 
+        }
+        return modalNotFound()
+    }
+    
+    function showModal(template) {                      // NOTE Muestra el resultado de la búsqueda
+        const $modal = document.getElementById('modal')
+        Configurando_y_mostrando_modal: {
+            $modal.classList.replace('no-modal','modal')// STUB Mostrando con animación fade-in
+            $modal.innerHTML = template
+            const image = $modal.querySelectorAll('img')
+            image[0].addEventListener('load', event => {
+                event.srcElement.classList.add('fadeIn')
+            })
+        }
+        Boton_cerrar: {
+            const $closeButton = document.getElementById('modal-close-Button')  // STUB Activa boton de cierre del modal
+            $closeButton.addEventListener('click', ()=>{   // NOTE 'click' listener
+                $modal.innerHTML = ""
+                $modal.classList.replace('modal', 'no-modal')
+            })
+        }
+    }
+
+    event.preventDefault()
+    let searchString       
+    
+    if (event.type === 'submit') {
+        const $buscador = event.target                   // NOTE const searchMovie = this.value (Pero me decido por otra forma)
+        const data = new FormData($buscador)             // STUB FormData, Se podría hacer también con qweryselector.contentBox
+        searchString = data.get('name')
+    } else if (event.type === 'click') {
+        searchString = event.target.parentElement.parentElement.dataset.title
+    }
+
+    let template = await searchMovie(searchString)
+    showModal(template)                                 // NOTE Muestra el modal con el resultado
+}                                                       //!SECTION 
+
+
+Cargando_script: {                                      // NOTE Cargando Script
+    async function load() {
+        window.localStorage.clear()
+        await loadLists()
+
+        const $buscador = document.getElementById("buscadorForm")
+        $buscador.addEventListener('submit', manageSearch)
+    }
+
+    window.addEventListener('load', load, false)
 }
-
-
-window.localStorage.clear()
-loadLists()
